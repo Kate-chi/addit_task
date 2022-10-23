0a341249f304ef75be4a45217d3c2a40a81187cf
Kate <c.chistova2014@yandex.ru>
2022-10-23 20:31:43 +0300

    бесконечный цикл

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿string ex = "exit";
+int x = 2;
+
+while(x > 1)
+{
+    Console.WriteLine("++++для выхода напишите exit++++");
+    if(ex == Console.ReadLine())
+    {
+        break;
+    }
+    x++;
+}
